02d62bac64c2050ae5adad2fa6bf5e9887465d77
cssrg-kmitl <67050737+cssrg-kmitl@users.noreply.github.com>
2022-10-10 14:48:36 +0700

    Update roticalculation.m

--- function/roticalculation.m
+++ function/roticalculation.m
@@ -1,47 +1,43 @@
 function ROTI = roticalculation(STECl,PRNall)
 %{
  ========================================
-            ROTI calculation
+            ROTI calculation (calculate every second)
  ========================================
+ 
  Description - Calculate Rate Of TEC Index (ROTI) 
  === input  ====
  STECl - Slant TEC that is calculated by using carrier-phase
  === output ====
  ROTI  - Rate Of TEC Index
 %}
-
+ROTI_sec = nan(size(STECl));
 for PRN = PRNall
-% 1440 mins devide by / windows 5 min
-% downsampling STEC to min
-STEC_minute     = STECl(1:60:length(STECl),PRN);
-
-% interpolate STEC min
-ST = find(~isnan(STEC_minute(:)));
-flag = find((diff(ST))>1 & (diff(ST))<=5); % flag nan value
-if ~isempty(flag)
-    for d = 1:length(flag)
-        x = [1,length(STEC_minute(ST(flag(d)):ST(flag(d)+1)))];    % Define start/stop epoch
-        v = [STEC_minute(ST(flag(d))),STEC_minute(ST(flag(d)+1))]; % Define start/stop data
-        xq = 1:length(STEC_minute(ST(flag(d)):ST(flag(d)+1)));     % Define interpolated epoch
-        STEC_M = interp1(x,v,xq,'linear','extrap');                % Interpolation 1D
+    % 1440 mins devide by / windows 5 min
+    STEC_sec = STECl(:,PRN);  
+    ST = find(~isnan(STEC_sec(:)));
+    flag = find((diff(ST))>1 & (diff(ST))<=300); % flag nan value
+    if ~isempty(flag)
+        for d = 1:length(flag)
+            x = [1,length(STEC_sec(ST(flag(d)):ST(flag(d)+1)))];    % Define start/stop epoch
+            v = [STEC_sec(ST(flag(d))),STEC_sec(ST(flag(d)+1))];    % Define start/stop data
+            xq = 1:length(STEC_sec(ST(flag(d)):ST(flag(d)+1)));     % Define interpolated epoch
+            STEC_M = interp1(x,v,xq,'linear','extrap');             % Interpolation 1D
         
-        STEC_minute(ST(flag(d)):ST(flag(d)+1)) = STEC_M;
+            STEC_sec(ST(flag(d)):ST(flag(d)+1)) = STEC_M;
+        end
+    end
+    
+    % downsampling STEC to min
+    for T = 300:length(STEC_sec) % 5 min
+        if isnan(STEC_sec(T))
+            ROTI_sec(T,PRN) = nan;
+        else
+            STEC_min = STEC_sec(T-299:60:T);
+            % Calculate different STEC
+            ROT            = diff(STEC_min);
+            ROT(length(ROT)+1)= NaN;
+            % Calculate standard deviation
+            ROTI_sec(T,PRN) = nanstd(ROT);
+        end
     end
 end
-
-
-% Calculate different STEC
-ROT            = diff(STEC_minute);
-ROT(length(ROT)+1)= NaN;
-% window 5 min
-window          = 5;
-time_rot = 1;
-% Calculate standard deviation
-for ind = 1 : window : length(ROT)- (window - 1)
-    ROT_Cal = ROT(ind:ind+(window-1));
-    ROTI(time_rot,PRN) = nanstd(ROT_Cal);
-    time_rot = time_rot +1;
-end
-% clear ST flag
-end
-% end
